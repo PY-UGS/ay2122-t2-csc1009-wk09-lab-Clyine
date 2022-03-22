5f19f250cf52d12f11d78c5d5353ca25d8bd7b8f
Jingquan <2101268@sit.singaporetech.edu.sg>
2022-03-22 09:41:26 +0800

    add format

--- Main.java
+++ Main.java
@@ -1,20 +1,25 @@
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Random Lowercase");
         for (int i = 0; i < 15 ; i++) {
             System.out.print(RandomCharacter.getRandomLowerCaseLetter() + " ");
         }
         System.out.println("");
 
+        System.out.println("Random Uppercase");
         for (int i = 0; i < 15 ; i++) {
             System.out.print(RandomCharacter.getRandomUpperCaseLetter() + " ");
         }
         System.out.println("");
 
+        System.out.println("Random Digit");
         for (int i = 0; i < 15 ; i++) {
             System.out.print(RandomCharacter.getRandomDigitCharacter() + " ");
         }
         System.out.println("");
 
+        System.out.println("Random Character");
         for (int i = 0; i < 15 ; i++) {
             System.out.print(RandomCharacter.getRandomCharacter() + " ");
         }
