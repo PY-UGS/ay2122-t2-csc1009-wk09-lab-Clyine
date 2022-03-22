470092d4b9ab08cba2210220959297c1c045fe63
Jingquan <2101268@sit.singaporetech.edu.sg>
2022-03-22 10:43:36 +0800

    many times

--- TestRandom.java
+++ TestRandom.java
@@ -1,7 +1,5 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
-
 
 public class TestRandom {
     private static String set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -9,28 +7,41 @@ public class TestRandom {
 
     @Test
     public void getRandomLowerCaseLetter() {
-        assertTrue("Random lower case is correct", RandomCharacter.getRandomLowerCaseLetter() >= 'a' && RandomCharacter.getRandomLowerCaseLetter() <= 'z');
+        for (int i = 0; i < 100; i++) {
+            assertTrue("Random lower case is correct", RandomCharacter.getRandomLowerCaseLetter() >= 'a'
+                    && RandomCharacter.getRandomLowerCaseLetter() <= 'z');
+        }
     }
 
     @Test
     public void getRandomUpperCaseLetter() {
-        assertTrue("Random Upper case is correct", RandomCharacter.getRandomUpperCaseLetter() >= 'A' && RandomCharacter.getRandomUpperCaseLetter() <= 'Z');
+        for (int i = 0; i < 100; i++) {
+            assertTrue("Random Upper case is correct", RandomCharacter.getRandomUpperCaseLetter() >= 'A'
+                    && RandomCharacter.getRandomUpperCaseLetter() <= 'Z');
+        }
     }
 
     @Test
     public void getRandomDigitCharacter() {
-        assertTrue("Random digit case is correct",RandomCharacter.getRandomDigitCharacter() >= '0' && RandomCharacter.getRandomDigitCharacter() <= '9');
+        for (int i = 0; i < 100; i++) {
+            assertTrue("Random digit case is correct", RandomCharacter.getRandomDigitCharacter() >= '0'
+                    && RandomCharacter.getRandomDigitCharacter() <= '9');
+        }
     }
 
     @Test
     public void getRandomChar() {
-        assertTrue("Random character case is correct",set.indexOf((int)RandomCharacter.getRandomLowerCaseLetter()) >= 0);
+        for (int i = 0; i < 100; i++) {
+            assertTrue("Random character case is correct",
+                    set.indexOf((int) RandomCharacter.getRandomLowerCaseLetter()) >= 0);
+        }
     }
 
     @Test
     public void isPrime() {
-        assertTrue("Random digit is Prime", primes.indexOf((int)RandomCharacter.getPrimeChracter()) >= 0);
+        for (int i = 0; i < 100; i++) {
+            assertTrue("Random digit is Prime", primes.indexOf((int) RandomCharacter.getPrimeChracter()) >= 0);
+        }
     }
 
-    
 }
